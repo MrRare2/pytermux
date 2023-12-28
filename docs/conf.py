893db57763ddbebed6370c2316c1ca8f7b3ee393
MrRare2 <125123977+MrRare2@users.noreply.github.com>
2023-12-28 09:35:45 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,35 @@
-print("done")
+project = "pytermux"
+author = "MrRare2"
+version = "1.0"
+full = version
+
+needs_sphinx = "2.0"
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib.spelling",
+    "sphinx_autodoc_typehints",
+]
+
+master_doc = "index.rst"
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+highlight_language = "python3"
+
+add_function_parentheses = False
+
+html_theme_options = {
+    "show_powered_by": False,
+    "github_user": "MrRare2",
+    "github_repo": project,
+    "github_banner": True,
+    "show_related": False,
+}
+
+html_show_sourcelink = False
+htmlhelp_basename = project + "doc"
+epub_exclude_files = ["search.html"]
+
+
